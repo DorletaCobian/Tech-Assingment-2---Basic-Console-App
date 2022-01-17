7cc611ae4665fb7524668c5d4051ee698c1df4d1
Dorleta Cobian <Dorleta Cobian@LAPTOP-OR0OJU2A>
2022-01-17 18:53:48 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// Dorleta Cobian Iregui
+using System;
+
+namespace Tech_Assingment_2___Basic_Console_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("It's not that we use techonolgy, we live technology. Submitted by Dorleta Cobian");
+        }
+    }
+}
